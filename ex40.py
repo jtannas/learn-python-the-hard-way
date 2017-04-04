315561a60c4e50382965f8eaf569120b26db72e4
Joel Tannas <jtannas@gmail.com>
2017-04-04 00:40:06 -0600

    Delete ex40.py

--- ex40.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2.7
-# pylint: disable = C0103
-"""exercise_title."""
-
-### End of Exercise. ##################################################
-# pylint: disable = W0105
-"""study_drills
-"""
